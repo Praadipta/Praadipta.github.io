98012cf829c0152f2f879b80a0f165c70fb91e09
Nightsviel <95136812+Praadipta@users.noreply.github.com>
2025-10-14 06:48:47 +0000

    changes in link

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -47,7 +47,7 @@ class Contact extends Component {
                 <div className="resume-btn-div">
                   <Button
                     text="See My Resume"
-                    newTab={true}
+                    newTab={false}
                     href={greeting.resumeLink}
                     theme={theme}
                   />
@@ -70,7 +70,7 @@ class Contact extends Component {
                 <div className="blogsite-btn-div">
                   <Button
                     text="Visit My Blogsite"
-                    newTab={true}
+                    newTab={false}
                     href={blogSection.link}
                     theme={theme}
                   />
